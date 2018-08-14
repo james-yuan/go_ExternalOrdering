9536e96b15393d8fd76b19f26a82de5c55275909
yuan.zk <2491042435@qq.com>
2018-08-15 01:22:43 +0800

    添加pub/sub模式

--- goroutine/main.go
+++ goroutine/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 	"github.com/bingfa/common"
+	"strings"
 )
 
 var startTime time.Time
@@ -39,7 +40,37 @@ func main() {
 	//search by goroutine使用场景
 	//  https://www.cnblogs.com/suoning/p/7237444.html
 
+	//pub/sub mode
+	pubsubMode();
+}
 
+func pubsubMode(){
+	//core: data structer
+	p := common.NewPublisher(100*time.Millisecond, 10)
+	defer p.Close()
+	//TODO
+	all := p.Subscribe()
+	golang := p.SubscribeTopic(func(v interface{}) bool {
+		if s, ok := v.(string); ok {
+			return strings.Contains(s, "golang")
+		}
+		return false
+	})
+	p.Publish("hello, world!")
+	p.Publish("hello, golang!")
+	go func() {
+		for msg := range all {
+			fmt.Println("all:", msg)
+		}
+	} ()
+
+	go func() {
+		for msg := range golang {
+			fmt.Println("golang:", msg)
+		}
+	} ()
+	// 运行一定时间后退出
+	time.Sleep(3 * time.Second)
 }
 
 func estChanType() {
